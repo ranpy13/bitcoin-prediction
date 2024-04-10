96d02bed7801f29344f68ecc06d981c8d68b932a
ranpy13 <85052085+ranpy13@users.noreply.github.com>
2024-04-10 10:41:49 +0530

    auto-arima best fit test

--- auto-arima.py
+++ auto-arima.py
@@ -1,3 +1,22 @@
 from base import *
+from pmdarima.arima import auto_arima
 
 # running auto-arima grid search to find the best model
+step_wise = auto_arima(
+    train_y,
+    exogenous = train_X,
+    start_p= 1,
+    start_q= 1,
+    max_p= 7,
+    max_q= 7,
+    d= 1,
+    max_d= 7,
+    trace= True,
+    m= 12,
+    error_action= 'ignore',
+    suppress_warnings= True,
+    stepwise= True
+)
+
+# print final results
+print(step_wise.summary())
